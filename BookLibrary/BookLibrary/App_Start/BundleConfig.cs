068c76bd1804ae61633c6e0d3f21e9cc0a7523d1
limbo93 <mhriad0@gmail.com>
2017-08-16 20:41:17 +0600

    Theme and Application name added

--- BookLibrary/BookLibrary/App_Start/BundleConfig.cs
+++ BookLibrary/BookLibrary/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace BookLibrary
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-cybrog.css",
                       "~/Content/site.css"));
         }
     }
